c2227bff4fcfa381b636a0586c46196fc7088f05
Andrew Kemp <akemp850@gmail.com>
2018-04-05 13:07:12 +0100

    Refactor to introduce sessions

--- lib/app.rb
+++ lib/app.rb
@@ -1,16 +1,23 @@
 require 'sinatra/base'
 
 class Battle < Sinatra::Application
-
+  set :session_secret, 'secret'
+  enable :sessions
 
   get '/' do
     erb :index
   end
 
   post '/battle' do
-  @player1 = params[:player1]
-  @player2 = params[:player2]
-  erb :play
+    session[:player1] = params[:player1]
+    session[:player2] = params[:player2]
+    redirect '/play'
+  end
+
+  get '/play' do
+    @player1 = session[:player1]
+    @player2 = session[:player2]
+    erb :play
   end
 
   run! if app_file == $0
